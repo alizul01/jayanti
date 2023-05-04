240ea6bf66c2c0869e36171378a1fbb56dea14eb
alizul01 <alizulfikar032@gmail.com>
2023-05-04 13:57:36 +0700

    fix(view): delete welcome view

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware(['guest'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
